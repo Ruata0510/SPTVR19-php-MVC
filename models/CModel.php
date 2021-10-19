99a02f076b7e2859e6983acd34907fb2c094312d
Ekaterina Lyamina <ekaterina_2001@inbox.ru>
2021-10-19 11:08:30 +0300

    new2 MVC

--- models/CModel.php
+++ models/CModel.php
@@ -2,6 +2,8 @@
 
 class Cmodel {
     public function GetData() {
+        
+        //1 вариант-------------------------------------------
         // $arrayResult[] = array(
         //     'TITLE' => 'Blog hosting site',
         //     'DATE' => '22.06.2020',
@@ -17,9 +19,27 @@ class Cmodel {
         //     'TEXT' => 'Vestibulum quis bibendum velit, eget iaculis odio. Ut cursus ipsum velit, ultricies accumsan magna varius tempus.',
         // );
 
-        $mysqli = new mysqli("localhost", "root", "", "sptvr19_php");
-        $result = $mysqli->query("SELECT * FROM blog_php");
-        $arrayResult = $result->fetch_all(MYSQLI_ASSOC);
+        //2 вариант--------------------------------------------
+        // $mysqli = new mysqli("localhost", "root", "", "sptvr19_php");
+        // $result = $mysqli->query("SELECT * FROM blog_php");
+        // $arrayResult = $result->fetch_all(MYSQLI_ASSOC);
+
+        //3 вариант--------------------------------------------
+        $host = '127.0.0.1';
+        $db = 'sptvr19_php';
+        $user = 'root';
+        $pass = '';
+        $charset = 'utf8';
+
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $opt = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+        $pdo = new PDO($dsn, $user, $pass, $opt);
+        $result = $pdo->query('SELECT * FROM blog_php');
+        $arrayResult = $result->fetchAll();
 
         return $arrayResult;
     }
